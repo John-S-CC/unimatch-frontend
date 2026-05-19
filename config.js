b4df6ac7e4e6e702de4080fa52188fc05511bffd
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-18 21:33:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Configuración local. En producción cambiar por el dominio real del backend.
-window.UNIMATCH_API_BASE = window.UNIMATCH_API_BASE || "https://unimatch-backend-fid5.onrender.com/api/login.php";
+window.UNIMATCH_API_BASE = window.UNIMATCH_API_BASE || "https://unimatch-backend-fid5.onrender.com/api/";
