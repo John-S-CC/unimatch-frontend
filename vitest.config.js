9f81484b1a55651a6bdbffda2999e4537645167c
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-23 18:16:24 -0500

    actualizacion vite

---
+++ vitest.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+
+    test: {
+
+        environment: 'jsdom',
+
+        coverage: {
+
+            provider: 'v8',
+
+            reporter: ['text', 'lcov'],
+
+            reportsDirectory: './coverage',
+
+            include: ['js/**/*.js']
+        }
+    }
+})
