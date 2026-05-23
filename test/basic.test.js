293763edf0b4a51cc30a4086126da670e08bc194
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-23 18:15:43 -0500

    coverage

---
+++ test/basic.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Basic frontend test', () => {
+
+    it('should work correctly', () => {
+
+        expect(true).toBe(true)
+
+    })
+
+})
